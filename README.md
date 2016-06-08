e4b3f8c96a25f34865264ce626b429fa3821a3cc
Rob Fielding <rob.fielding@gmail.com>
2016-06-08 06:17:03 -0400

    add json query language

--- README.md
+++ README.md
@@ -2,3 +2,46 @@
 
 Given logs generated with uber-go zap, and piped in via stdin, and some naming conventions on events,
 generate statistical logs from the incoming events.
+
+
+The collect is a stdin program that takes a json argument from bash.
+This is the ./runit script:
+```
+#!/bin/bash
+
+go build && ( 
+cat data.json | go run collect.go -command='{
+  "echo":false,
+  "gstat":true,
+  "stat":false,
+  "selected":[
+    "msg",
+    "session",
+    "counter",
+    "throughput",
+    "latency"
+  ]
+}'
+)
+```
+
+Data input looks like uber-go/zap json serializer output.  It will optionally deal with a token in front, as comes with docker-compose:
+```
+docker_odrive_1 {"msg":"txn begin","level":"info","ts":100,"fields":{"session":"6"}}
+docker_odrive_1 {"msg":"txn begin","level":"info","ts":2013,"fields":{"session":"43"}}
+docker_odrive_1 {"msg":"txn up","level":"info","ts":3023,"fields":{"session":"43","bytes":104813}}
+docker_odrive_1 {"msg":"txn begin","level":"info","ts":5000,"fields":{"session":"42"}}
+docker_odrive_1 {"msg":"txn down","level":"info","ts":5300,"fields":{"session":"42","bytes":5123}}
+docker_odrive_1 {"msg":"txn end","level":"info","ts":10000,"fields":{"session":"42"}}
+docker_odrive_1 {"msg":"txn end","level":"info","ts":50324,"fields":{"session":"43"}}
+docker_odrive_1 {"msg":"txn up","level":"info","ts":90300,"fields":{"session":"6","bytes":3248923}}
+docker_odrive_1 {"msg":"txn end","level":"info","ts":100324,"fields":{"session":"6"}}
+```
+
+Executing ./runit will produce output according to how the script was configured:
+```	
+{"msg"="gstat","counter"="txn up","throughput"="22.578759","latency"="74267.500000"}
+{"msg"="gstat","counter"="txn down","throughput"="1.024600","latency"="5000.000000"}
+rfielding@rfielding-Inspiron-3521:~/goworkspace/src/github.com/rfielding/collect$ 
+
+```
--- collect.go
+++ collect.go
@@ -4,9 +4,17 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/uber-go/zap"
 	"os"
+	"strings"
+)
+
+const (
+	TString  = 1
+	TInt64   = 2
+	TFloat64 = 3
 )
 
 var (
@@ -28,7 +36,141 @@ type GStat struct {
 	Count     int64
 }
 
+//Duplicating this because zap's members are private.  :-(
+type NamedParm struct {
+	Key        string
+	VType      int
+	StringVal  string
+	Int64Val   int64
+	Float64Val float64
+}
+
+func String(k string, v string) NamedParm {
+	return NamedParm{
+		Key:       k,
+		VType:     TString,
+		StringVal: v,
+	}
+}
+
+func Int64(k string, v int64) NamedParm {
+	return NamedParm{
+		Key:      k,
+		VType:    TInt64,
+		Int64Val: v,
+	}
+}
+
+func Float64(k string, v float64) NamedParm {
+	return NamedParm{
+		Key:        k,
+		VType:      TFloat64,
+		Float64Val: v,
+	}
+}
+
+func Val(p NamedParm) string {
+	switch p.VType {
+	case TString:
+		return p.StringVal
+	case TInt64:
+		return fmt.Sprintf("%d", p.Int64Val)
+	case TFloat64:
+		return fmt.Sprintf("%f", p.Float64Val)
+	}
+	return ""
+}
+
+func ToZap(params ...NamedParm) []zap.Field {
+	var zapFields []zap.Field
+	for _, p := range params {
+		switch p.VType {
+		case TString:
+			zapFields = append(zapFields, zap.String(p.Key, p.StringVal))
+		case TInt64:
+			zapFields = append(zapFields, zap.Int64(p.Key, p.Int64Val))
+		case TFloat64:
+			zapFields = append(zapFields, zap.Float64(p.Key, p.Float64Val))
+		}
+	}
+	return zapFields
+}
+
+var command = flag.String("command", "{}", "json command config")
+var commandStructure interface{}
+var doEchoInput = false
+var doGstat = true
+var doStat = false
+var doSelected []string
+
+func commandSetup() error {
+	flag.Parse()
+	commandBuffer := bytes.NewBuffer([]byte(*command))
+	commandDecoder := json.NewDecoder(commandBuffer)
+	commandDecoder.UseNumber()
+	err := commandDecoder.Decode(&commandStructure)
+	if err != nil {
+		return err
+	}
+	topLevel, topLevel_ok := commandStructure.(map[string]interface{})
+	if topLevel_ok {
+		echo, echo_ok := topLevel["echo"].(bool)
+		if echo_ok {
+			doEchoInput = echo
+		}
+		gstat, gstat_ok := topLevel["gstat"].(bool)
+		if gstat_ok {
+			doGstat = gstat
+		}
+		stat, stat_ok := topLevel["stat"].(bool)
+		if stat_ok {
+			doStat = stat
+		}
+		selected, selected_ok := topLevel["selected"].([]interface{})
+		if selected_ok {
+			for _, v := range selected {
+				vStr, vStr_ok := v.(string)
+				if vStr_ok {
+					doSelected = append(doSelected, vStr)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+//standard json marshaling doesn't preserve key order, which we must do, because
+//we are essentially rendering a table
+func render(msg string, fields ...NamedParm) {
+	if len(doSelected) > 0 {
+		fields = append(fields, String("msg", msg))
+		w := os.Stdout
+		w.Write([]byte("{"))
+		i := 0
+		for _, s := range doSelected {
+			for _, f := range fields {
+				if s == f.Key {
+					if i > 0 {
+						w.Write([]byte(","))
+					}
+					w.Write([]byte(fmt.Sprintf("\"%s\"=\"%s\"", f.Key, Val(f))))
+					i++
+				}
+			}
+		}
+		w.Write([]byte("}\n"))
+	} else {
+		logger.Info(msg, ToZap(fields...)...)
+	}
+}
+
 func main() {
+	err := commandSetup()
+	if err != nil {
+		logger.Error("command setup", zap.String("err", err.Error()))
+		os.Exit(-1)
+	}
+
 	gstats := make(map[string]*GStat)
 	gstats["txn up"] = &GStat{Direction: "txn up"}
 	gstats["txn down"] = &GStat{Direction: "txn down"}
@@ -37,12 +179,16 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
+		if len(line) > 0 && line[0] != '{' {
+			idx := strings.Index(line, " ")
+			line = line[idx+1:]
+		}
 		if line != "" {
 			var obj interface{}
 			lineBuffer := bytes.NewBuffer([]byte(line))
 			decoder := json.NewDecoder(lineBuffer)
 			decoder.UseNumber()
-			err := decoder.Decode(&obj)
+			err = decoder.Decode(&obj)
 			if err != nil {
 				logger.Error(
 					"parse fail",
@@ -50,16 +196,15 @@ func main() {
 					zap.String("line", line),
 				)
 			} else {
-				fmt.Println(line)
-				logger.Debug("json")
+				if doEchoInput {
+					fmt.Println(line)
+				}
 				record, record_ok := obj.(map[string]interface{})
 				if record_ok {
-					logger.Debug("record")
 					ts, ts_ok := record["ts"].(json.Number)
 					msg, msg_ok := record["msg"].(string)
 					fields, fields_ok := record["fields"].(map[string]interface{})
 					if msg_ok && fields_ok && ts_ok {
-						logger.Debug("session")
 						session, session_ok := fields["session"].(string)
 						tsVal, err := ts.Int64()
 						if err != nil {
@@ -68,29 +213,28 @@ func main() {
 						if session_ok {
 							switch msg {
 							case "txn begin":
-								logger.Debug("begin")
 								txns[session] = &Txn{Session: session}
 								txns[session].Session = session
 								txns[session].Begin = tsVal
 							case "txn end":
-								logger.Debug("end")
 								txns[session].End = tsVal
 								txn := txns[session]
 								diff := float64(txn.End - txn.Begin)
 								xput := float64(txn.Bytes) / diff
-								logger.Info(
-									"txn",
-									zap.Float64("throughput", xput),
-									zap.String("session", session),
-									zap.String("direction", txn.Direction),
-									zap.Int64("latency", txn.End-txn.Begin),
-								)
+								if doStat {
+									render(
+										"txn",
+										Float64("throughput", xput),
+										String("session", session),
+										String("counter", txn.Direction),
+										Int64("latency", txn.End-txn.Begin),
+									)
+								}
 								txns[session] = nil
 								gstats[txn.Direction].Bytes += txn.Bytes
 								gstats[txn.Direction].Diff += txn.End - txn.Begin
 								gstats[txn.Direction].Count += int64(1)
 							case "txn down", "txn up":
-								logger.Debug("bytes")
 								bytes, bytes_ok := fields["bytes"].(json.Number)
 								if bytes_ok {
 									bytesVal, err := bytes.Int64()
@@ -107,16 +251,18 @@ func main() {
 			}
 		}
 	}
-	for k, v := range gstats {
-		xput := float64(v.Bytes) / float64(v.Diff)
-		latency := float64(v.Diff) / float64(v.Count)
-		bytes := float64(v.Bytes) / float64(v.Count)
-		logger.Info(
-			"gstat",
-			zap.String("direction", k),
-			zap.Float64("throughput", xput),
-			zap.Float64("latency", latency),
-			zap.Float64("bytes", bytes),
-		)
+	if doGstat {
+		for k, v := range gstats {
+			xput := float64(v.Bytes) / float64(v.Diff)
+			latency := float64(v.Diff) / float64(v.Count)
+			bytes := float64(v.Bytes) / float64(v.Count)
+			render(
+				"gstat",
+				String("counter", k),
+				Float64("throughput", xput),
+				Float64("latency", latency),
+				Float64("bytes", bytes),
+			)
+		}
 	}
 }
